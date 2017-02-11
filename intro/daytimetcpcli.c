9c0a61c1417652360719968796439af101ae07b0
xuguangmin <345253580@qq.com>
2017-02-12 01:00:51 +0800

    daytimetcpcli.c

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -14,6 +14,21 @@ main(int argc, char **argv)
 		err_sys("socket error");
 	
 	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(13);
+	if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+		err_quit("inet_pton error for %s", argv[1]);
+	
+	if(connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) < 0)
+		err_sys("connect error");
 
+	while( (n = read(sockfd, recvline, MAXLINE)) > 0) {
+		recvline[n] = 0;
+		if(fputs(recvline, stdout) == EOF)
+			err_sys("fputs error");
+	}
+
+	if(n < 0)
+		err_sys("read_error");
 	return 0;
 }
--- lib/unp.h
+++ lib/unp.h
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <sys/types.h>          /* See NOTES */
+ #include <arpa/inet.h>
 
 
 #define HAVE_VSNPRINTF
 
 #define MAXLINE	4096
+
+#define SA	struct sockaddr
+
+
 void err_quit(const char *, ...);
+void err_sys(const char *, ...);
 
